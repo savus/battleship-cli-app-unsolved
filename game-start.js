d25dbeda593888d743704c5bf66bd05569c11844
savus <justin.fuzzygreenstuff@gmail.com>
2025-01-15 10:46:17 -0700

    test createBoard function

--- game-start.js
+++ game-start.js
@@ -6,8 +6,20 @@ const testBoard1 = {
       { type: "large", id: 1, hit: true },
       { type: "large", id: 1, hit: true },
       { type: "large", id: 1, hit: true },
+      { type: "large", id: 1, hit: true },
+      { type: "large", id: 1, hit: true },
+      { type: "large", id: 1, hit: true },
+      { type: "large", id: 1, hit: true },
+      { type: "large", id: 1, hit: true },
+      { type: "large", id: 1, hit: true },
     ],
     B: [
+      { type: "large", id: 1, hit: true },
+      { type: "large", id: 1, hit: true },
+      { type: "large", id: 1, hit: true },
+      { type: "large", id: 1, hit: true },
+      { type: "large", id: 1, hit: true },
+      { type: "large", id: 1, hit: true },
       { type: "large", id: 1, hit: true },
       { type: "large", id: 1, hit: true },
       { type: "large", id: 1, hit: true },
@@ -16,8 +28,47 @@ const testBoard1 = {
       { type: "large", id: 1, hit: true },
       { type: "large", id: 1, hit: true },
       { type: "large", id: 1, hit: true },
+      { type: "large", id: 1, hit: true },
+      { type: "large", id: 1, hit: true },
+      { type: "large", id: 1, hit: true },
+      { type: "large", id: 1, hit: true },
+      { type: "large", id: 1, hit: true },
+      { type: "large", id: 1, hit: true },
     ],
   },
 };
 
-console.table(testBoard1.grid);
+const testBoardDisplay = {
+  grid: {
+    A: ["🔳", "🔳", "🔳", "🔳", "🔳", "🔳"],
+    B: ["🔳", "🔳", "🔳", "🔳", "🔳", "🔳"],
+    C: ["🔳", "🔳", "🔳", "🔳", "🔳", "🔳"],
+    D: ["🔳", "🔳", "🔳", "🔳", "🔳", "🔳"],
+    E: ["🔳", "🔳", "🔳", "🔳", "🔳", "🔳"],
+    F: ["🔳", "🔳", "🔳", "🔳", "🔳", "🔳"],
+  },
+};
+
+const createBoard = (size) => {
+  const board = {
+    size: size,
+    grid: {},
+  };
+
+  for (let i = 0; i < board.size; i++) {
+    board.grid[alphabet[i]] = [];
+    for (let j = 0; j < board.size; j++) {
+      board.grid[alphabet[i]][j] = { type: "large", id: 1, hit: false };
+    }
+  }
+
+  return board;
+};
+
+const printBoard = (board, debug) => {
+  const boardDisplay = {};
+};
+
+const board1 = createBoard(3);
+
+console.log(board1.grid["A"].map((cell) => cell.type));
