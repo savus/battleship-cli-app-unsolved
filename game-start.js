04abf5d34d64f64896eeae245bff258c53cd82c6
savus <justin.fuzzygreenstuff@gmail.com>
2025-01-15 10:33:54 -0700

    working on grid

--- game-start.js
+++ game-start.js
@@ -1,1 +1,23 @@
-console.log("Successfully restart");
+const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+const testBoard1 = {
+  grid: {
+    A: [
+      { type: "large", id: 1, hit: true },
+      { type: "large", id: 1, hit: true },
+      { type: "large", id: 1, hit: true },
+    ],
+    B: [
+      { type: "large", id: 1, hit: true },
+      { type: "large", id: 1, hit: true },
+      { type: "large", id: 1, hit: true },
+    ],
+    C: [
+      { type: "large", id: 1, hit: true },
+      { type: "large", id: 1, hit: true },
+      { type: "large", id: 1, hit: true },
+    ],
+  },
+};
+
+console.table(testBoard1.grid);
