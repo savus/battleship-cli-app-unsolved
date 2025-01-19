f931561d63e61be3654b017fbb8abf6029d95956
savus <justin.fuzzygreenstuff@gmail.com>
2025-01-19 09:13:47 -0700

    condensed blocks of code in playTurn function into smaller components

--- game-functions.js
+++ game-functions.js
@@ -16,7 +16,37 @@ export const isShipDead = (ship) => ship.lives === 0;
 export const checkIfWon = (shipList) =>
   shipList.filter((ship) => ship.lives > 0) === 0;
 
+export const shipIsSunk = (hitShip, shipList) => {
+  if (isShipDead(hitShip)) {
+    const remainingShips = shipList.filter((ship) => ship.lives > 0);
+    readlineSync.question(`${hitShip.name} has been sunk!`);
+    if (remainingShips.length === 0) {
+      readlineSync.question("Congrats! You won!");
+      return true;
+    } else {
+      readlineSync.question(`${remainingShips.length} ships remaining!`);
+    }
+  }
+  return false;
+};
+
+export const shipIsHit = (board, shipList, str) => {
+  let gameIsOver = false;
+  const hitShip = findShip(shipList, str);
+  setCell(board, str, {
+    type: hitShip.type,
+    id: hitShip.id,
+    hit: true,
+  });
+  readlineSync.question("You made a hit!");
+  hitShip.subtractLives(1);
+  gameIsOver = shipIsSunk(hitShip, shipList);
+  if (gameIsOver) return true;
+  return false;
+};
+
 const gameMain = (board, str, shipList) => {
+  let gameIsOver = false;
   if (areCoordsValid(board, str)) {
     if (isLocationAlreadyHit(board, str)) {
       readlineSync.question("This location has already been hit");
@@ -25,27 +55,12 @@ const gameMain = (board, str, shipList) => {
         setCell(board, str, { type: "empty", id: 0, hit: true });
         readlineSync.question("Sorry, you missed!");
       } else {
-        const hitShip = findShip(shipList, str);
-        setCell(board, str, {
-          type: hitShip.type,
-          id: hitShip.id,
-          hit: true,
-        });
-        readlineSync.question("You made a hit!");
-        hitShip.subtractLives(1);
-        if (isShipDead(hitShip)) {
-          const remainingShips = shipList.filter((ship) => ship.lives > 0);
-          readlineSync.question(`${hitShip.name} has been sunk!`);
-          if (remainingShips.length === 0) {
-            readlineSync.question("Congrats! You won!");
-            return;
-          } else {
-            readlineSync.question(`${remainingShips.length} ships remaining!`);
-          }
-        }
+        gameIsOver = shipIsHit(board, shipList, str);
+        if (gameIsOver) return true;
       }
     }
   }
+  return false;
 };
 
 export const playTurn = (board, shipList, debug) => {
@@ -57,7 +72,6 @@ export const playTurn = (board, shipList, debug) => {
       debug ? "exit" : "enter"
     } debug mode]\n`
   );
-
   const cleanStrCopy = removeSpacesAndSpecialChars(userInput).toUpperCase();
 
   if (cleanStrCopy.toLowerCase() === "quit") {
@@ -66,7 +80,9 @@ export const playTurn = (board, shipList, debug) => {
     debug = !debug;
     return playTurn(board, shipList, debug);
   } else {
-    gameMain(board, cleanStrCopy, shipList);
+    let gameIsOver = gameMain(board, cleanStrCopy, shipList);
+    if (gameIsOver) return;
   }
+
   return playTurn(board, shipList, debug);
 };
